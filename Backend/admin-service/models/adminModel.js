56cf77973b5584461dc5f6492e6daf9c8976a1e7
dmisyuk <david101cccp@gmail.com>
2025-10-11 13:29:46 -0400

    added  createevent to adminModel

--- Backend/admin-service/models/adminModel.js
+++ Backend/admin-service/models/adminModel.js
@@ -7,3 +7,12 @@ const db = new sqlite3.Database('tixDB.sql', (err) => {
             console.log('Connected to the SQLite database.');
         }
     });
+
+    function createEvent(event_name, event_date, event_tickets, event_location){
+     const query = 'INSERT INTO Event (event_name, event_date, event_tickets, event_location) VALUES (?, ?, ?, ?)';
+
+     db.run(query, [event_name, event_date, event_tickets, event_location]);
+
+    }
+
+    module.exports = {createEvent};
