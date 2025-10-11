c700b32328b95b6bb95de9d046a8980a9a4f3d7a
dmisyuk <david101cccp@gmail.com>
2025-10-11 12:46:56 -0400

    changed databse name and setup.

--- Backend/admin-service/models/adminModel.js
+++ Backend/admin-service/models/adminModel.js
@@ -1,2 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
-const dbName = ""
+
+const db = new sqlite3.Database('tixDB.sql', (err) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log('Connected to the SQLite database.');
+        }
+    });
