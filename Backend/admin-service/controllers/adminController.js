8bb69f0e17cf88bfa5fa5787239446800d33845e
Aeterna1283 <rb9362123@gmail.com>
2025-10-11 17:29:05 -0400

    minor issue in admin control

--- Backend/admin-service/controllers/adminController.js
+++ Backend/admin-service/controllers/adminController.js
@@ -7,7 +7,7 @@ async function createEvent(req, res){
 
         const newEvent = await adminModel.createEvent(event_name, event_date, event_tickets, event_location);    
         //const newTicket = await adminModel.createTicket(newEvent.event_id, true, ticket_price, ticket_type);
-        res.status(201).json({newEvent});
+        res.status(201).json(newEvent);
     } catch (err) {
         console.error("Failed to create event: ", err.message);
         res.status(500).json({error: "Server error"})
