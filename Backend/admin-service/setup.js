02b000e679322802733babf22ffc203071825b4d
dmisyuk <david101cccp@gmail.com>
2025-10-11 16:21:40 -0400

    working on admin setup

--- Backend/admin-service/setup.js
+++ Backend/admin-service/setup.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, '../../shared-db/database.sqlite');
+const dbPath = path.join(__dirname, '../shared-db/database.sqlite');
 const db = new sqlite3.Database(dbPath, (err) => {
         if (err) {
             console.error('Connection error with database', err.message);
@@ -10,8 +10,10 @@ const db = new sqlite3.Database(dbPath, (err) => {
         }
     });
 
-
+db.serialize(() => {
     db.run('PRAGMA foreign_keys = ON');
+    db.run('DROP TABLE IF EXISTS Event');
     db.run('DROP TABLE IF EXISTS Ticket');
     db.run('CREATE TABLE Event (event_id INTEGER PRIMARY KEY AUTOINCREMENT, event_name TEXT NOT NULL, event_date TEXT NOT NULL, event_tickets INTEGER NOT NULL, event_location TEXT NOT NULL)');
     db.run('CREATE TABLE Ticket (ticket_id INTEGER PRIMARY KEY AUTOINCREMENT, event_id INTEGER NOT NULL, ticket_price REAL, ticket_type TEXT, ticket_availability BOOLEAN NOT NULL, FOREIGN KEY(event_id) REFERENCES Event(event_id)');
+});
