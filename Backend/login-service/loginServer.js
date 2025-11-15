4eea9c4c02bf06585e95bdcd7710d3ae4f5bdd72
Aeterna1283 <rb9362123@gmail.com>
2025-11-15 16:38:47 -0500

    fixed loginserver.js

--- Backend/login-service/loginServer.js
+++ Backend/login-service/loginServer.js
@@ -1,43 +1,29 @@
-// const express = require('express');
-// const cors = require('cors');
-// const authRoutes = require('./routes/llmRoute');
-
-// const app = express();
-// const PORT = 8001;
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/api/login', authRoutes);
-
-// app.listen(PORT, () =>
-// {
-//     console.log('Login service running on http://localhost:${PORT');
-// });
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const loginRoutes = require("./routes/loginRoutes");
+const registerRoutes = require("./routes/registerRoutes");
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// DATABASE
-const db = new sqlite3.Database(process.env.DATABASE_PATH, (err) => {
+const db = new sqlite3.Database(process.env.DATABASE_PATH || "./users.sqlite", (err) => 
+{
   if (err) console.error("DB Error:", err);
-  else console.log("Login DB connected.");
+  else console.log("Auth DB connected.");
 });
 
 app.set("db", db);
 
-// ROUTES
+app.use("/api/register", registerRoutes);
 app.use("/api/login", loginRoutes);
 
-// START SERVER
 const PORT = process.env.PORT || 8001;
-app.listen(PORT, () => {
-  console.log(`Login service running on port ${PORT}`);
+app.listen(PORT, () => 
+{
+  console.log(`Auth service running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
